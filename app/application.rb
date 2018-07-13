29a56629f654b25446c43f942cb1b3a1e858b6ee
David Moore <hey@iamdavidmoore.com>
2018-07-13 13:39:49 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -20,8 +20,8 @@ class Application
         resp.write "#{item}\n"
       end
 
-    elsif req.path.match(/add/)
-      selection = req.params["item"]
+    # elsif req.path.match(/add/)
+    #   selection = req.params["item"]
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
